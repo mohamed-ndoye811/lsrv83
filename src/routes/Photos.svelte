84ea18bc0cc0452e253614944663124db90d7174
mohamed-ndoye811 <59741838+mohamed-ndoye811@users.noreply.github.com>
2021-03-14 17:19:17 +0100

    Update

---
+++ src/routes/Photos.svelte
@@ -0,0 +1,25 @@
+<script>
+  import Carousel from "@beyonk/svelte-carousel";
+
+  function importAll(r) {
+    return r.keys().map(r);
+  }
+
+  const images = importAll(require.context("./", false, /\.(png|jpe?g|svg)$/));
+</script>
+
+<div class="carouselContainer">
+  <Carousel perPage={3} loop={true}>
+    <div class="slide-content">Slide 1</div>
+    <div class="slide-content">Slide 2</div>
+    <div class="slide-content">Slide 3</div>
+    <div class="slide-content">Slide 4</div>
+  </Carousel>
+</div>
+
+<style>
+  .carouselContainer {
+    width: 100%;
+    height: 100%;
+  }
+</style>
